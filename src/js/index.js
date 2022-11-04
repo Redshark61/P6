fd9b2d73bcf1a477e32e03faf927d1276ae2d2e7
Thimothée VANIER <tva@subteno.com>
2022-11-04 14:09:51 +0100

    dropdown for tag button

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,55 @@
-const $tagBtns = document.querySelectorAll(".tag-wrapper > button");
+const $tagBtns = document.querySelectorAll(".tag-wrapper > span");
+const $is = Array.from($tagBtns).map((e) => e.querySelector("i"));
+const buttons = {
+	"tag-tools": "Ustensiles",
+	"tag-ingredients": "Ingrédients",
+	"tag-machine": "Appareils",
+};
 
 $tagBtns.forEach(($tagBtn) => {
 	$tagBtn.addEventListener("click", (e) => {
-		$tagBtn.parentElement.classList.toggle("active");
-		const $list = $tagBtn.nextElementSibling;
-		const width = $list.clientWidth;
-		if ($tagBtn.parentElement.classList.contains("active")) {
-			const $classes = $tagBtn.classList;
-			const $input = document.createElement("input");
-			$input.setAttribute("type", "text");
-			$input.classList = $classes;
-			$input.style.width = `${width}px`;
-			$input.placeholder = `Rechercher un ${$tagBtn.textContent.trim().toLowerCase()}`;
-			$tagBtn.replaceWith($input);
-		} else {
-			$tagBtn.setAttribute("style", "");
-			$tagBtn.replaceWith(document.createElement("button"));
-		}
+		console.log("click on span 1");
+		toggle($tagBtn);
 	});
 });
+
+function toggle($tagBtn) {
+	let $parent;
+	let $list;
+	let $button;
+	let $span;
+	if ($tagBtn.tagName === "I") {
+		$parent = $tagBtn.parentElement.parentElement;
+		$list = $tagBtn.parentElement.nextElementSibling;
+		$button = $tagBtn.previousElementSibling;
+		$span = $tagBtn.parentElement;
+	} else {
+		$parent = $tagBtn.parentElement;
+		$list = $tagBtn.nextElementSibling;
+		$button = $tagBtn.querySelector("button");
+		$span = $tagBtn;
+	}
+
+	$parent.classList.toggle("active");
+	const width = $list.clientWidth;
+
+	if ($parent.classList.contains("active")) {
+		const $input = document.createElement("input");
+		$input.classList.add("no-btn", "input-light");
+		$input.setAttribute("type", "text");
+		$input.placeholder = `Rechercher un ${$tagBtn.textContent.trim().toLowerCase()}`;
+		$span.style.width = `${width}px`;
+		$button.replaceWith($input);
+		$input.addEventListener("click", (e) => {
+			e.stopPropagation();
+		});
+	} else {
+		$span.style.width = "auto";
+		const $button = document.createElement("button");
+		$button.classList.add("no-btn", "c-white");
+		$button.textContent =
+			buttons[Array.from($span.classList).filter((c) => c.startsWith("tag-"))[0]];
+		const $input = $span.querySelector("input");
+		$input.replaceWith($button);
+	}
+}
